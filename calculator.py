2f578d894943320a57b2ecafb269a55fe568174c
Safayethtml <safayetbiswas233@gmail.com>
2026-03-04 14:48:15 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# Sum Calculator
+#F1=int(input("Enter First Value:"))
+#F2=int(input("Enter Second Value:"))
+
+#print("Total:", F1 * F2)
+"""
+# Multipication
+Value1= int(input("Enter Value 1:"))
+Value2= int(input("Enter Value 2:"))
+print("The sum is",Value1 * Value2)
+"""
+"""
+# Substruction 
+ValueA= int(input("Enter First Value:"))
+ValueB= int(input("Enter second Value:"))
+print("The Substraction is", ValueA-ValueB)
+"""
+
+#division
+ValueA=int(input("enter value of a:"))
+ValueB=int(input("enter value of b:"))
+print("Division of A and B is:", ValueA/ValueB)
